e1ac93eb9076380e9b95bf32c8ccc28b88cfe446
pmnxis <pmnxis@gmail.com>
2021-08-21 19:28:21 +0900

    Confirm it works

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,32 @@
-#![no_std]
-#![no_main]
 #![deny(warnings)]
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
 
 extern crate cortex_m;
 extern crate cortex_m_rt as rt;
-extern crate panic_semihosting;
-extern crate rtic;
+extern crate nb;
+extern crate panic_halt;
 extern crate stm32g0xx_hal as hal;
 
-use cortex_m_semihosting::hprintln;
-use hal::gpio;
-use hal::gpio::{Output, PushPull};
 use hal::prelude::*;
-// use hal::rtc::Rtc;
-// use hal::stm32::{self, Interrupt};
-use hal::stm32::{self};
-use hal::timer::Timer;
-use rtic::app;
+use hal::stm32;
+use nb::block;
+use rt::entry;
 
-#[app(device = hal::stm32, peripherals = true)]
-const APP: () = {
-    struct Resources {
-        heartbeat_timer: Timer<stm32::TIM16>,
-        heartbeat: gpio::gpioc::PC6<Output<PushPull>>,
+#[entry]
+fn main() -> ! {
+    let dp = stm32::Peripherals::take().expect("cannot take peripherals");
+    let mut rcc = dp.RCC.constrain();
+
+    let gpioc = dp.GPIOC.split(&mut rcc);
+    let mut led = gpioc.pc6.into_push_pull_output();
+
+    let mut timer = dp.TIM17.timer(&mut rcc);
+    timer.start(500.ms());
+
+    loop {
+        led.toggle().unwrap();
+        block!(timer.wait()).unwrap();
     }
-
-    // Just borrowed from examples for now.
-
-    #[init]
-    fn init(ctx: init::Context) -> init::LateResources {
-        let mut rcc = ctx.device.RCC.constrain();
-        // rtic::pend(Interrupt::USART2);
-
-        let gpioc = ctx.device.GPIOC.split(&mut rcc);
-
-        let mut heartbeat_timer = ctx.device.TIM16.timer(&mut rcc);
-        heartbeat_timer.start(3.hz());
-        heartbeat_timer.listen();
-
-        hprintln!("Hello Rust").unwrap();
-
-        init::LateResources {
-            heartbeat_timer,
-            heartbeat: gpioc.pc6.into_push_pull_output(),
-        }
-    }
-
-    #[task(binds = TIM16, resources = [heartbeat, heartbeat_timer])]
-    fn timer_tick(ctx: timer_tick::Context) {
-        ctx.resources.heartbeat.toggle().unwrap();
-        ctx.resources.heartbeat_timer.clear_irq();
-    }
-};
+}
