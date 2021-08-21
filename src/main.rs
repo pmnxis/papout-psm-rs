f9d572ee2e2321ffb48d8737220de28bec33fb74
pmnxis <pmnxis@gmail.com>
2021-08-21 21:31:47 +0900

    Not working one (backup for investigation)

--- src/main.rs
+++ src/main.rs
@@ -6,55 +6,63 @@ extern crate cortex_m;
 extern crate cortex_m_rt as rt;
 extern crate panic_semihosting;
 extern crate rtic;
+extern crate nb;
 extern crate stm32g0xx_hal as hal;
 
 use cortex_m_semihosting::hprintln;
-use hal::exti::Event;
 use hal::gpio;
-use hal::gpio::{Output, PushPull, SignalEdge};
+use hal::gpio::{Output, PushPull};
 use hal::prelude::*;
 // use hal::rtc::Rtc;
 // use hal::stm32::{self, Interrupt};
 use hal::stm32::{self};
+use hal::rcc::Config;
+use hal::rcc::Prescaler;
 use hal::timer::Timer;
+use nb::block;
 use rtic::app;
+
 
 #[app(device = hal::stm32, peripherals = true)]
 const APP: () = {
     struct Resources {
-        exti: stm32::EXTI,
-        exti_indicator: gpio::gpiob::PB9<Output<PushPull>>,
+        indicator_timer: Timer<stm32::TIM17>,
+        indicator: gpio::gpiob::PB9<Output<PushPull>>,
         heartbeat_timer: Timer<stm32::TIM16>,
         heartbeat: gpio::gpiob::PB8<Output<PushPull>>,
     }
 
-    // Just borrowed from examples for now.
-
     #[init]
+    #[allow(unused_mut)]
     fn init(mut ctx: init::Context) -> init::LateResources {
-        let mut rcc = ctx.device.RCC.constrain();
-        // rtic::pend(Interrupt::USART2);
+    let mut rcc = ctx.device.RCC.freeze(
+        Config::hsi(Prescaler::NotDivided));
 
-        let gpioa = ctx.device.GPIOA.split(&mut rcc);
-        let gpiob = ctx.device.GPIOB.split(&mut rcc);
+    let gpiob = ctx.device.GPIOB.split(&mut rcc);
 
-        let mut heartbeat_timer = ctx.device.TIM16.timer(&mut rcc);
-        heartbeat_timer.start(3.hz());
-        heartbeat_timer.listen();
+    let mut heartbeat_timer = ctx.device.TIM16.timer(&mut rcc);
+    heartbeat_timer.start(500.ms());
+    heartbeat_timer.listen();
 
-        gpioa.pa4.listen(SignalEdge::All, &mut ctx.device.EXTI);
-        // gpioa.pa5.listen(SignalEdge::All, &mut ctx.device.EXTI);
-        // gpioa.pa6.listen(SignalEdge::All, &mut ctx.device.EXTI);
+    
+    let mut indicator_timer = ctx.device.TIM17.timer(&mut rcc);
+    indicator_timer.start(500.ms());
 
-        // let mut rtc = ctx.device.RTC.constrain(&mut rcc);
-
-        hprintln!("Hello Rust").unwrap();
+    hprintln!("Hello Rust").unwrap();
 
         init::LateResources {
-            exti: ctx.device.EXTI,
-            exti_indicator: gpiob.pb9.into_push_pull_output(),
+            indicator_timer,
+            indicator: gpiob.pb9.into_push_pull_output(),
             heartbeat_timer,
             heartbeat: gpiob.pb8.into_push_pull_output(),
+        }
+    }
+
+    #[idle(resources = [indicator, indicator_timer])]
+    fn idle(ctx: idle::Context) -> ! {
+        loop {
+            ctx.resources.indicator.toggle().unwrap();
+            block!(ctx.resources.indicator_timer.wait()).unwrap();
         }
     }
 
@@ -62,12 +70,5 @@ const APP: () = {
     fn timer_tick(ctx: timer_tick::Context) {
         ctx.resources.heartbeat.toggle().unwrap();
         ctx.resources.heartbeat_timer.clear_irq();
-    }
-
-    #[task(binds = EXTI4_15, resources = [exti, exti_indicator])]
-    fn button_click(ctx: button_click::Context) {
-        hprintln!("Button pressed").unwrap();
-        ctx.resources.exti.unpend(Event::GPIO14);
-        ctx.resources.exti_indicator.toggle().unwrap();
     }
 };
