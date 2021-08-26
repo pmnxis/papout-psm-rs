8a2db9bc9896310dfd3370f17cf5d27a3a917d3f
pmnxis <pmnxis@gmail.com>
2021-08-27 05:22:03 +0900

    without isr, fifo in USART2

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,10 @@ extern crate cortex_m_rt as rt;
 extern crate panic_halt;
 extern crate stm32g0xx_hal as hal;
 
-use core::fmt::Write;
-
 use hal::prelude::*;
 use hal::serial::*;
 use hal::stm32;
+use nb::block;
 use rt::entry;
 use rtt_target::{rprintln, rtt_init_print};
 
@@ -26,39 +25,18 @@ fn main() -> ! {
         .usart(
             gpioa.pa2,
             gpioa.pa3,
-            FullConfig::default()
-                .baudrate(9600.bps())
-                .fifo_enable()
-                .rx_fifo_enable_interrupt()
-                .rx_fifo_threshold(FifoThreshold::FIFO_4_BYTES),
+            BasicConfig::default().baudrate(9600.bps()),
             &mut rcc,
         )
         .unwrap();
 
     rtt_init_print!();
-    rprintln!("Hello USART2\n");
-
-    let (mut tx1, mut rx1) = usart2.split();
+    rprintln!("Hello USART2\r\n");
 
     let mut cnt = 0;
     loop {
-        if rx1.fifo_threshold_reached() {
-            loop {
-                match rx1.read() {
-                    Err(nb::Error::WouldBlock) => {
-                        // no more data available in fifo
-                        break;
-                    }
-                    Err(nb::Error::Other(_err)) => {
-                        // Handle other error Overrun, Framing, Noise or Parity
-                        rprintln!("nb::Error::Other(_err)) =>");
-                    }
-                    Ok(byte) => {
-                        rprintln!("{}: {}\n", cnt, byte);
-                        cnt += 1;
-                    }
-                }
-            }
-        }
+        let byte = block!(usart2.read()).unwrap();
+        rprintln!("{}: {}\r", cnt, byte);
+        cnt += 1;
     }
 }
