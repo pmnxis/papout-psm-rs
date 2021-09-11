5fbe9bfe65a2c0d587db03a5917101198e4ecf2d
pmnxis <pmnxis@gmail.com>
2021-09-11 15:09:43 +0900

    to_core_data on serial request

--- src/obdl1000_serial_request.rs
+++ src/obdl1000_serial_request.rs
@@ -1,6 +1,40 @@
 #![no_std]
 #![no_main]
 
+macro_rules! cap_u8 {
+    ($foo: expr, $is_signed: expr) => {
+        ($foo & 0x20) | (0x20 * (!$is_signed as u8))
+    };
+}
+
+macro_rules! caparr_TTT {
+    ($a0: expr, $a1: expr, $a2: expr, $is_signed: expr) => {
+        [
+            cap_u8!($a0, $is_signed),
+            cap_u8!($a1, $is_signed),
+            cap_u8!($a2, $is_signed),
+        ]
+    };
+}
+
+macro_rules! caparr_TTd {
+    ($a0: expr, $a1: expr, $a2: expr, $is_signed: expr) => {
+        [cap_u8!($a0, $is_signed), cap_u8!($a1, $is_signed), $a2]
+    };
+}
+
+macro_rules! caparr_Tdd {
+    ($a0: expr, $a1: expr, $a2: expr, $is_signed: expr) => {
+        [cap_u8!($a0, $is_signed), $a1, $a2]
+    };
+}
+
+macro_rules! caparr_TdT {
+    ($a0: expr, $a1: expr, $a2: expr, $is_signed: expr) => {
+        [cap_u8!($a0, $is_signed), $a1, cap_u8!($a2, $is_signed)]
+    };
+}
+
 pub enum SerialRequest {
     SayHi,
     Init,
@@ -44,6 +78,21 @@ impl SerialRequest {
             (b'S' | b's', 0x00, 0x00) => Ok(SerialRequest::StateCheck),
             (b'S', b'E', b'R') | (b's', b'e', b'r') => Ok(SerialRequest::ErrorCheck),
             _ => Err(Error::WrongCommand),
+        }
+    }
+
+    fn to_core_data(&self, capital: bool) -> [u8; 3] {
+        match self {
+            &Self::SayHi => caparr_TTd!(b'H', b'I', b'?', capital),
+            &Self::Init => caparr_Tdd!(b'I', 0x00, 0x00, capital),
+            &Self::Dispense(byte) => caparr_TdT!(b'D', byte, b'S', capital),
+            &Self::HaltAction => caparr_Tdd!(b'H', 0x00, 0x00, capital),
+            &Self::HaltActionCancel => caparr_TTd!(b'H', b'C', b'?', capital),
+            &Self::RemoveCount => caparr_TTT!(b'R', b'E', b'M', capital),
+            &Self::GetTotalDispensed => caparr_TTd!(b'G', b'T', b'?', capital),
+            &Self::RemoveTotalCount => caparr_TTT!(b'C', b'T', b'C', capital),
+            &Self::StateCheck => caparr_Tdd!(b's', 0x00, 0x00, capital),
+            &Self::ErrorCheck => caparr_TTT!(b'S', b'E', b'R', capital),
         }
     }
 
